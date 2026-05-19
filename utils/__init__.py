98023642269979581a58f9011f1e062db3d107a0
Thierry Huot <thuot2@uwo.ca>
2026-05-19 18:31:47 -0400

    feat: implement spatial evaluation metrics and batch processor with dummy testing

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,8 @@
 """Shared utilities for the methane detection pipeline."""
+from .metrics import calculate_iou, calculate_dice, calculate_spatial_fdr
+
+__all__ = [
+    'calculate_iou',
+    'calculate_dice',
+    'calculate_spatial_fdr'
+]
